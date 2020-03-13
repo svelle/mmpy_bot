b9d86bb69ed468836b7996f6a22a52dfcd0799f5
Paul Schiffer <knister94@gmail.com>
2020-03-13 19:36:07 +0100

    Add plot_last_three_months()

--- mmpy_bot/stock_lookup.py
+++ mmpy_bot/stock_lookup.py
@@ -1,6 +1,8 @@
-from yfinance import Ticker
+from yfinance import Ticker, download
 import json
 import requests
+from datetime import date, timedelta
+import matplotlib.pyplot as plt
 
 
 def look_up_stock(symbol):
@@ -29,3 +31,13 @@ def find_symbol_name(query):
         name=symbol_candidate["name"],
         exchange=symbol_candidate["exch"],
     )
+
+
+def plot_last_three_months(symbol, currency="US-Dollar"):
+    today = date.today()
+    three_months_ago = today - timedelta(days=90)
+    data = download(symbol, three_months_ago, today)
+    data["Adj Close"].plot(title=f"{symbol} - Last three months")
+    plt.ylabel(currency)
+    plt.savefig("plot.png")
+    return "plot.png"
