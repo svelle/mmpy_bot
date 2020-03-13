a961d5b87097424bea8faf68069fdcfc77d69007
Sven Huester <sven.svelle@gmail.com>
2020-03-13 23:10:39 +0100

    formatting

--- mmpy_bot/plugins/stonks.py
+++ mmpy_bot/plugins/stonks.py
@@ -3,21 +3,38 @@
 import re
 
 from mmpy_bot.bot import listen_to
+from mmpy_bot.bot import respond_to
 
 import mmpy_bot.stock_lookup as stock
+
+
+@respond_to('help', re.IGNORECASE)
+def help(message):
+    content = ("\n"
+               "#### Stonkbot for maximum Stonks!!!\n"
+               "#### Available Commands:\n"
+               "- `get info`: Gets info for a given stock symbol.\n"
+               "- `get symbol`: Tries to get the symbol for a given company name.\n"
+               )
+    message.reply_thread(content)
 
 
 @listen_to('get symbol', re.IGNORECASE)
 def get_symbol(message):
     msg_inp = message.get_message().split()
-    name = str(msg_inp[-1]).upper()
+    name = str(msg_inp[-1])
 
     search_result = stock.find_symbol_name(name)
-
-    content = f"""\n ### Symbol: {search_result['symbol']}
-#### Company Name: {search_result['name']}
-#### Exchange: {search_result['exchange']}
-"""
+    if search_result:
+        content = ("\n"
+                   f"### Symbol: {search_result['symbol'].upper()}\n"
+                   f"#### Company Name: {search_result['name']}\n"
+                   f"#### Exchange: {search_result['exchange']}\n"
+                   )
+    else:
+        content = (f"\n#### Couldn't find info for {name}, "
+                   "try another search string."
+                   )
     message.reply_thread(content)
 
 
@@ -36,18 +53,19 @@ def get_info(message):
 
     stock_image = stock.plot_last_three_months(symbol, stock_info['currency'])
 
-    content = f"""\n### Company Name: [{stock_info['name']}](https://finance.yahoo.com/quote/{symbol}])
-### Symbol: {symbol}
-
-| Previous Close |{stock_info['previous_close']}|
-| ---------------- | --- |
-| Open           |{stock_info['open']}|
-| Bid            |{stock_info['bid']}|
-| Ask            |{stock_info['ask']}|
-| Beta           |{stock_info['beta']}|
-| Day's Range    |{stock_info['days_range']}|
-| 52 Week Range  |{stock_info['weeks_range']}|
-"""
+    content = ("\n"
+               f"### Company Name: [{stock_info['name']}](https://finance.yahoo.com/quote/{symbol}])\n"
+               f"### Symbol: {symbol}\n\n"
+               "\n"
+               f"| Previous Close |{stock_info['previous_close']}|\n"
+               "| ---------------- | --- |\n"
+               f"| Open           |{stock_info['open']}|\n"
+               f"| Bid            |{stock_info['bid']}|\n"
+               f"| Ask            |{stock_info['ask']}|\n"
+               f"| Beta           |{stock_info['beta']}|\n"
+               f"| Day's Range    |{stock_info['days_range']}|\n"
+               f"| 52 Week Range  |{stock_info['weeks_range']}|\n"
+               )
 
     file = open(stock_image, "rb")
     result = message.upload_file(file)
@@ -59,5 +77,4 @@ def get_info(message):
     message.reply_thread(content, [file_id])
 
 
-get_symbol.__doc__ = "Try's to lookup the symbol for a given company name."
-get_info.__doc__ = "Try's to lookup data for the given symbol."
+help.__doc__ = "Shows the available commands for Stonkbot."
